13d9a9406697245b6853eb24c024acc8dab1776d
Mohamed-Khalil67 <mohamed.khalil.ucbl@gmail.com>
2026-09-02 23:48:01 +0300

    jo

--- src/app/recipes/recipe-list/recipe-list.ts
+++ src/app/recipes/recipe-list/recipe-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RecipeItem } from "./recipe-item/recipe-item";
+import { Recipe } from '../recipe.model';
 
 @Component({
   imports: [RecipeItem],
@@ -7,4 +8,22 @@ import { RecipeItem } from "./recipe-item/recipe-item";
   styleUrl: './recipe-list.scss',
   templateUrl: './recipe-list.html',
 })
-export class RecipeList { }
+export class RecipeList {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe 1',
+      'This is test 1 description for recipe',
+      'assets/recipes-book.png'
+    ),
+    new Recipe(
+      'Test recipe 1',
+      'This is test 1 description for recipe',
+      'assets/recipes-book.png'
+    ),
+    new Recipe(
+      'Test recipe 1',
+      'This is test 1 description for recipe',
+      'assets/recipes-book.png'
+    )
+  ]
+}
